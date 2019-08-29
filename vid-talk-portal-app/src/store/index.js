a8a2978e3ecc3746dfbbd4a24454e5835075fff4
chashikajw <chashikajw007@gmail.com>
2019-08-30 03:45:38 +0530

    add store

---
+++ vid-talk-portal-app/src/store/index.js
@@ -0,0 +1,23 @@
+import {  createStore, applyMiddleware,compose} from 'redux';
+import reducer from './reducer';
+import thunk from 'redux-thunk'
+
+
+const IntialStates = {
+    services: {name:'fukkkk'},
+    users : []
+}
+
+const middleware = [thunk]
+
+const store = createStore(
+    reducer, IntialStates,
+    compose(applyMiddleware(...middleware),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    
+   );
+
+console.log(store.getState());
+
+export default store;
+
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
